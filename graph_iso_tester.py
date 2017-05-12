ec9a383ac25aa1ec2e7c0d512ea36c21f0aa99f0
gabriela1604 <gabriela.georgieva1604@gmail.com>
2017-05-12 23:49:16 +0200

    Further work on iso tester class

--- graph_iso_tester.py
+++ graph_iso_tester.py
@@ -9,25 +9,27 @@ class GraphIsoTester(object):
 
     def __init__(self, graph_g, graph_h):
         """
-        TODO: docstring
+        Initialize class with instances
+        G and H - networkx graphs,
+        and the disjoint union of both
 
-        :param graph_g:
-        :param graph_h:
-        :return:
+        :param graph_g: networkx graph
+        :param graph_h: networkx graph
+        :return: GraphIsoTester object
         """
         self.G = graph_g
         self.H = graph_h
         self.disjoint_union = nx.disjoint_union(self.G, self.H)
 
     def is_balanced(self):
-        """ Validates if the degree of the edges
+        """ Validates if the degree of the nodes
          are equal in both graphs
 
         :return: boolean
         """
-        labels_g = Counter(self.G.degree().values())
-        labels_h = Counter(self.H.degree().values())
-        if labels_g == labels_h:
+        degrees_g = Counter(self.G.degree().values())
+        degrees_h = Counter(self.H.degree().values())
+        if degrees_g == degrees_h:
             return True
         else:
             return False
@@ -43,7 +45,42 @@ class GraphIsoTester(object):
             return False
         elif self.G.number_of_edges() != self.H.number_of_edges():
             return False
-        elif self.is_balanced() is False:
+        elif not self.is_balanced():
             return False
+
         return True
 
+    def n_degree_hood(self, graph, node, degree=1):
+        """
+        Get the n-degree neighborhood of a node
+        :param graph. networkx graph
+        :param node: node of the graph
+        :param degree: int: default value 1
+        :return: list, containing the n-degree neighbors of node
+        """
+        shortest_paths = nx.single_source_dijkstra_path_length(
+            graph, node
+        )
+        result = [node for node, path_length in shortest_paths.iteritems()
+                  if path_length == degree]
+        return result
+
+    def one_step_color_refignment(self, colors):
+        """TODO: docstring
+        :param colors:
+        :return:
+        """
+        # get initial colors of the disjoint union
+        pass
+
+    def color_refignment(self, X):
+        """
+
+        :param X: nx graph
+        :return: graph X is colored such that
+        no to neighbor nodes have the same color
+        """
+        # initial colors = degree of the nodes
+        initial_colors = X.degree()
+        # node and all its neighbors
+        n_hood = {node: X.neighbors(node) for node in X.nodes()}
